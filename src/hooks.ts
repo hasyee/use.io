6d7fc756ece2b0da822669b638391436a4816a41
hasyee <hasyee@gmail.com>
2020-03-07 20:51:32 +0100

    fix updater signature

--- src/hooks.ts
+++ src/hooks.ts
@@ -6,14 +6,16 @@ import {
   Store,
   Selector,
   Setter,
-  Updater,
   Assignments,
   Combiner,
   Dependencies,
+  ValueOrReducer,
   IO
 } from './store';
 
-export type Hook<T> = () => [T, Setter<T>];
+export type Updater<T> = (...args: any[]) => ValueOrReducer<T>;
+
+export type Hook<T, U = Setter<T>> = () => [T, U];
 
 export type SelectorHook<T> = () => T;
 
@@ -25,45 +27,44 @@ export type HookDependencies<D1, D2> = [Hook<D1>, Hook<D2>?];
 
 type IUse = {
   <T, D1, D2>(store: Selector<T, D1, D2>): T;
-  <T>(store: Store<T>): [T, Setter<T>];
+  <T>(store: Store<T>, updater?: Updater<T>): [T, typeof updater];
 };
 
-const use: IUse = (store: any): any => {
+const use: IUse = (store: any, updater?: any): any => {
   const update = useMemo(
-    () => ('set' in store && 'update' in store ? <A>(...args: A[]) => store.set!(store.update!(...args)) : store.set),
-    [store]
+    () => (!!store.set && !!updater ? <A>(...args: A[]) => store.set!(updater(...args)) : store.set),
+    [store, updater]
   );
   const [state, setState] = useState(store.get());
   useEffect(() => store.subscribe(setState), [setState]);
   return update ? [state, update] : state;
 };
 
-const useNot: IUse = (store: any): any => {
-  const update =
-    'set' in store && 'update' in store ? <A>(...args: A[]) => store.set!(store.update!(...args)) : store.set;
+const useNot: IUse = (store: any, updater?: any): any => {
+  const update = !!store.set && !!updater ? <A>(...args: A[]) => store.set!(updater!(...args)) : store.set;
   return update ? [store.get(), update] : store.get();
 };
 
 const wrap: {
   <T, D1, D2>(store: Selector<T, D1, D2>): SelectorHook<T>;
-  <T>(store: Store<T>): Hook<T>;
-} = (store: any): any => {
-  const useStore = (useHook: boolean = true) => (useHook ? use(store) : useNot(store));
+  <T>(store: Store<T>, updater?: Updater<T>): Hook<T, typeof updater>;
+} = (store: any, updater?: any): any => {
+  const useStore = (useHook: boolean = true) => (useHook ? use(store, updater) : useNot(store, updater));
   Object.defineProperty(useStore, IO, { value: store, configurable: false, enumerable: false, writable: false });
   return useStore;
 };
 
-const createStateHook = <T>(initialState: T, update?: Updater<T>) => wrap(createStore(initialState, update));
+const createStateHook = <T>(initialState: T, updater?: Updater<T>) => wrap(createStore(initialState), updater);
 
-const createCompositeHook = <T>(hookAssignments: HookAssignments<T>, update?: Updater<T>) =>
+const createCompositeHook = <T>(hookAssignments: HookAssignments<T>, updater?: Updater<T>) =>
   wrap(
     createCompositeStore(
       Object.keys(hookAssignments).reduce(
         (acc, key) => ({ ...acc, [key]: hookAssignments[key][IO] }),
         {} as Assignments<T>
-      ),
-      update
-    )
+      )
+    ),
+    updater
   );
 
 const createSelectorHook = <T, D1, D2>(combiner: Combiner<T, D1, D2>, hookDependencies: HookDependencies<D1, D2>) =>
