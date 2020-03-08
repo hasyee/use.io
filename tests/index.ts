b89f2c5d9d49d0a36635911090638706f2a096cc
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-03-08 15:28:07 +0100

    io.state

--- tests/index.ts
+++ tests/index.ts
@@ -1,11 +1,11 @@
 import io from '..';
 
-const useCounter = io(10);
+const useCounter = io.state(10);
 const [count, setCount] = useCounter();
 setCount(count + 1);
 useCounter(false);
 
-const useCounter2 = io(10, (value1: number, value2: number) => state => state + value1 + value2);
+const useCounter2 = io.state(10, (value1: number, value2: number) => state => state + value1 + value2);
 const [count2, setCount2] = useCounter2();
 setCount2(count2 + 1, 2);
 useCounter2(false);
