46c03ae7bf578ee259a80dd5c7d4397682ae3ba9
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-03-08 13:57:11 +0100

    add Hydrator

---
+++ src/hydrate.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { getStoreHooks, getStoresFromHooks, getPrimitiveStores, getState } from './helpers';
+
+const HYDRATOR_ID = '__USE_IO_HYDRATOR__';
+
+export const getHydrator = hooks => {
+  const stores = getPrimitiveStores(getStoresFromHooks(getStoreHooks(hooks)));
+  const isServer = typeof window === 'undefined';
+  if (isServer) {
+    return () =>
+      React.createElement('input', { id: HYDRATOR_ID, type: 'hidden', value: JSON.stringify(getState(stores)) });
+  } else {
+    const hiddenInput = window.document.getElementById(HYDRATOR_ID);
+    if (!hiddenInput) return;
+    const hydratedState = JSON.parse(hiddenInput.value);
+    Object.keys(hydratedState).forEach(key => stores[key].set(hydratedState[key]));
+    return () => {
+      const [isRendered, setIsRendered] = useState(true);
+      useEffect(() => {
+        setIsRendered(false);
+      }, []);
+      return (
+        isRendered &&
+        React.createElement('input', { id: HYDRATOR_ID, type: 'hidden', value: JSON.stringify(getState(stores)) })
+      );
+    };
+  }
+};
