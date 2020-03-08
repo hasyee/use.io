e203b8ad641c3e92883445808179d0b7d70403f1
hasyee <hasyee@gmail.com>
2020-03-08 14:38:30 +0100

    refactor hydrate

--- src/hydrate.js
+++ src/hydrate.js
@@ -2,27 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { getStoreHooks, getStoresFromHooks, getPrimitiveStores, getState } from './helpers';
 
 const HYDRATOR_ID = '__USE_IO_HYDRATOR__';
+let stores;
+
+const isServer = typeof window === 'undefined';
 
 export const hydrate = hooks => {
-  const stores = getPrimitiveStores(getStoresFromHooks(getStoreHooks(hooks)));
-  const isServer = typeof window === 'undefined';
-  if (isServer) {
-    return () =>
-      React.createElement('input', { id: HYDRATOR_ID, type: 'hidden', value: JSON.stringify(getState(stores)) });
-  } else {
+  stores = getPrimitiveStores(getStoresFromHooks(getStoreHooks(hooks)));
+
+  if (!isServer) {
     const hiddenInput = window.document.getElementById(HYDRATOR_ID);
     if (!hiddenInput) return;
     const hydratedState = JSON.parse(hiddenInput.value);
     Object.keys(hydratedState).forEach(key => stores[key].set(hydratedState[key]));
-    return () => {
-      const [isRendered, setIsRendered] = useState(true);
-      useEffect(() => {
-        setIsRendered(false);
-      }, []);
-      return (
-        isRendered &&
-        React.createElement('input', { id: HYDRATOR_ID, type: 'hidden', value: JSON.stringify(getState(stores)) })
-      );
-    };
   }
 };
+
+export const Hydrator = React.memo(function Hydrator() {
+  const [isRendered, setIsRendered] = useState(true);
+  useEffect(() => {
+    setIsRendered(false);
+  }, []);
+  if (!isRendered) return null;
+  return React.createElement('input', { id: HYDRATOR_ID, type: 'hidden', value: JSON.stringify(getState(stores)) });
+});
