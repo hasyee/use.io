a76878b60f072551c6973c8d4447a1e59a6ffadf
hasyee <hasyee@gmail.com>
2020-03-23 15:34:55 +0100

    refactor to current

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import * as hooks from './hooks';
+import * as store from './store';
 import * as updaters from './updaters';
 import * as dev from './dev';
 import * as hydrate from './hydrate';
 
-export * from './hooks';
+export * from './store';
 export * from './updaters';
 export * from './dev';
 export * from './hydrate';
 
-export default { ...hooks, ...updaters, ...dev, ...hydrate };
+export default { ...store, ...updaters, ...dev, ...hydrate };
