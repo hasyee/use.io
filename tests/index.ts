ed890172a5f98868e8033bc6c9e2c1a53678cb96
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-03-25 06:06:06 +0100

    update type definitions

--- tests/index.ts
+++ tests/index.ts
@@ -1,35 +1,16 @@
 import io from '..';
 
-const useCounter = io.state(10);
-const [count, setCount] = useCounter();
-setCount(count + 1);
-useCounter(false);
+const counter = io.state(10, {
+  increase: (value: number) => state => state + value
+});
+const useCounter = counter.hook();
+const [{ increase, set: setCount }, count] = useCounter();
 
-const useCounter2 = io.state(10, (value1: number, value2: number) => state => state + value1 + value2);
-const [count2, setCount2] = useCounter2();
-setCount2(count2 + 1, 2);
-useCounter2(false);
-
-const useComp = io.compose({ count1: useCounter, count2: useCounter2 });
-const [comp, setComp] = useComp();
-setComp({ count1: comp.count1 + 1, count2: comp.count2 + 1 });
-useComp(false);
-
-const useComp2 = io.compose({ count1: useCounter, count2: useCounter2 }, (count1: number, count2: number) => state => ({
-  ...state,
-  count1,
-  count2
-}));
-const [comp2, setComp2] = useComp2();
-setComp2(comp2.count1 + 1, comp2.count2 + 1);
-useComp2(false);
-
-const useIsOdd = io.memo((n, e) => Boolean(n % 2), [useCounter]);
-const isOdd = useIsOdd();
-isOdd === true;
-useIsOdd(false);
-
-const useIsEven = io.memo(isOdd => !isOdd, [useIsOdd]);
-const isEven = useIsEven();
-isEven === true;
-useIsEven(false);
+const comp = io.compose(
+  { count: counter },
+  {
+    increase2: (value: number) => state => ({ ...state, count: state.count + value })
+  }
+);
+const useComp = comp.hook();
+const [{ set: setComp, increase2 }, compValue] = useComp();
